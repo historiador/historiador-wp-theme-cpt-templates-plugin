0a0c69de1b3f7f7e80d9cfee7204bbdd24638fd7
Protesilaos Stavrou <info@protesilaos.com>
2018-11-19 18:04:48 +0200

    New repo for the GitHub organisation

---
+++ README.txt
@@ -0,0 +1,12 @@
+Historiador Custom Post Types Templates Plugin
+==============================================
+
+
+THIS IS WORK IN PROGRESS.  The following document has not been updated
+for a while.  Whatever information is presented herein may be false,
+incomplete, or outdated.  Updates shall, noetheless, be delivered on a
+regular basis from now on (starting 2018-11-20).
+
+
+
+This is a companion plugin for the Historiador WordPress Theme.
---
+++ sidebar-videos.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+// NOTE see functions.php `register_sidebar()`
+if ( ! is_active_sidebar( 'sidebar-7' ) ) {
+	return;
+}
+?>
+
+<aside id="secondary" class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Podcast Sidebar', 'historiador' ); ?>">
+	<?php dynamic_sidebar( 'sidebar-7' ); ?>
+</aside><!-- #secondary -->
